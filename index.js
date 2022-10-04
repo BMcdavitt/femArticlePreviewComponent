b54e89d0e52e35d4e1a8f2bbe34c8e45834b4630
BMcDavitt <bill.mcdavitt@yahoo.com>
2022-10-03 20:51:04 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,69 @@
+function toggleFooterOn() {
+  const profileFooter = document.getElementById('footerProfile')
+  const footerShare = document.getElementById('footerShare')
+  const footer = document.getElementById('footer')
+  const shareIcon = document.getElementById('shareIcon')
+  const shareIconHighlight = document.getElementById('shareIconHighlight')
+  const tooltip = document.getElementById('tooltip')
+  const tooltipNipple = document.getElementById('tooltipNipple')
+  const width = window.innerWidth
+  const height = window.innerHeight
+
+  if (width <= 800) {
+    profileFooter.style.display = 'none'
+    footerShare.style.display = 'flex'
+    shareIcon.style.display = 'none'
+    shareIconHighlight.style.display = 'block'
+
+    footer.style.animation = 'changeColor 1s forwards'
+    footerShare.style.animation = 'changeColor 1s forwards'
+  } else {
+    let hPosition = width / 2 + 200
+    let vPosition = height / 2 - 35
+    let nhPosition = hPosition + 115
+    let nvPosition = vPosition + 50
+    tooltip.style.left = hPosition + 'px'
+    tooltip.style.top = vPosition + 'px'
+    tooltip.style.opacity = 1
+    tooltipNipple.style.left = nhPosition + 'px'
+    tooltipNipple.style.top = nvPosition + 'px'
+    tooltipNipple.style.opacity = 1
+  }
+}
+
+function toggleFooterOff() {
+    const profileFooter = document.getElementById('footerProfile')
+    const footerShare = document.getElementById('footerShare')
+    const footer = document.getElementById('footer')
+    const shareIcon = document.getElementById('shareIcon')
+    const shareIconHighlight = document.getElementById('shareIconHighlight')
+    const tooltip = document.getElementById('tooltip')
+    const tooltipNipple = document.getElementById('tooltipNipple')
+    const width = window.innerWidth
+    const height = window.innerHeight
+
+  if (width <= 800) {
+    footerShare.style.display = 'none'
+    profileFooter.style.display = 'flex'
+    shareIcon.style.display = 'block'
+    shareIconHighlight.style.display = 'none'
+
+    footer.style.animation = 'revertColor 1s forwards'
+    footerShare.style.animation = 'revertColor 1s forwards'
+  } else {
+    tooltip.style.opacity = 0
+    tooltipNipple.style.opacity = 0
+  }
+}
+
+let footerShareActive = false
+
+function toggleFooter() {
+  if (footerShareActive) {
+    toggleFooterOff()
+    footerShareActive = false
+  } else {
+    toggleFooterOn()
+    footerShareActive = true
+  }
+}
